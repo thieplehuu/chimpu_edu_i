bda7418e73e88201a77fbcf94fff631c1d1dd7cc
Le Huu Thiep <le_huu_thiep@management-partners.co.jp>
2020-01-03 17:02:45 +0700

    add file

---
+++ lib/pages/top/top.dart
@@ -0,0 +1,222 @@
+import 'package:chimpu_edu_i/core/res/assets.dart';
+import 'package:chimpu_edu_i/core/res/functions.dart';
+import 'package:chimpu_edu_i/core/res/text_styles.dart';
+import 'package:chimpu_edu_i/core/widgets/announcement_slider.dart';
+import 'package:chimpu_edu_i/core/widgets/rounded_bordered_container.dart';
+import 'package:chimpu_edu_i/data/model/announcement.dart';
+import 'package:chimpu_edu_i/data/model/survey.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class TopPage extends StatefulWidget {
+  @override
+  _TopPageState createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage>
+    with SingleTickerProviderStateMixin {
+  bool dialogShowing;
+  bool showNewUiDialog;
+  AnimationController _animationController;
+  List<Announcement> announcements;
+  SurveyItem survey;
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 800),
+    );
+    dialogShowing = false;
+    showNewUiDialog = false;
+    //_getRemoteConfig();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: customAppBar(context: context),
+      body: ListView(
+        children: <Widget>[
+          RoundedContainer(
+            borderRadius: BorderRadius.circular(10.0),
+            margin: const EdgeInsets.symmetric(
+              vertical: 8.0,
+              horizontal: 16.0,
+            ),
+            padding: const EdgeInsets.all(0),
+            height: 250,
+            child: InkWell(
+              borderRadius: BorderRadius.circular(10.0),
+              onTap: () => Navigator.pushNamed(context, 'challenge_home'),
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10.0),
+                        topRight: Radius.circular(10.0),
+                      ),
+                      child: Image.asset(
+                        appFeatureImage,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        "UI Challenges",
+                        style: buttonText.copyWith(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      const SizedBox(width: 10.0),
+                      Icon(
+                        Icons.arrow_forward,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10.0),
+                ],
+              ),
+            ),
+          ),
+          if (announcements.length > 0) ...[
+            AnnouncementSlider(news: announcements),
+            const SizedBox(height: 16.0),
+          ],
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                CategoryItem(
+                  icon: Icon(
+                    FontAwesomeIcons.user,
+                    color: Colors.indigo,
+                  ),
+                  title: "Profile",
+                  onPressed: () {
+                  },
+                ),
+                const SizedBox(height: 10.0),
+                CategoryItem(
+                  title: "About",
+                  icon: Icon(
+                    FontAwesomeIcons.infoCircle,
+                    color: Colors.red,
+                  ),
+                  onPressed: () => Navigator.pushNamed(context, 'about'),
+                ),
+                if (survey != null &&
+                    user != null &&
+                    !user.surveys.contains(survey?.id)) ...[
+                  const SizedBox(height: 10.0),
+                  SurveyWidget(survey: survey),
+                ],
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryItem extends StatelessWidget {
+  final TextStyle buttonText = boldText.copyWith(
+    fontSize: 16.0,
+  );
+  final Function onPressed;
+  final String title;
+  final Widget icon;
+
+  CategoryItem({
+    Key key,
+    this.onPressed,
+    this.title,
+    this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      highlightElevation: 0,
+      elevation: 0,
+      padding: const EdgeInsets.all(24.0),
+      child: Row(
+        children: <Widget>[
+          if (icon != null) ...[
+            icon,
+            const SizedBox(width: 10.0),
+          ],
+          Text(
+            title,
+            style: buttonText,
+          ),
+          Spacer(),
+          Icon(Icons.keyboard_arrow_right),
+        ],
+      ),
+      color: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      onPressed: onPressed,
+    );
+  }
+}
+
+class FeaturedCategoryItem extends StatelessWidget {
+  final TextStyle buttonText = boldText.copyWith(
+    fontSize: 16.0,
+  );
+  final Function onPressed;
+  final String title;
+  final Widget icon;
+
+  FeaturedCategoryItem({
+    Key key,
+    this.onPressed,
+    this.title,
+    this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      colorBrightness: Brightness.dark,
+      highlightColor: Theme.of(context).primaryColor,
+      highlightElevation: 0,
+      elevation: 0,
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
+      child: Row(
+        children: <Widget>[
+          if (icon != null) ...[
+            icon,
+            const SizedBox(width: 10.0),
+          ],
+          Text(
+            title,
+            style: buttonText.copyWith(
+                fontSize: 24.0, fontWeight: FontWeight.w300),
+          ),
+          Spacer(),
+          Icon(Icons.keyboard_arrow_right),
+        ],
+      ),
+      color: Theme.of(context).primaryColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      onPressed: onPressed,
+    );
+  }
+}
