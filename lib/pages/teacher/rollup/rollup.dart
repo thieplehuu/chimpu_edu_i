a8ca677b9b595c083183f6907474590c7d00eafd
Le Huu Thiep <thieplehuugt@gmail.com>
2020-01-05 18:25:31 +0700

    add rollup page

---
+++ lib/pages/teacher/rollup/rollup.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class RollUpPage extends StatefulWidget {
+  const RollUpPage({Key key}) : super(key: key);
+
+  @override
+  _RollUpPageState createState() => _RollUpPageState();
+}
+
+class _RollUpPageState extends State<RollUpPage> with TickerProviderStateMixin {
+  static final String path = "lib/pages/teacher/home/home.dart";
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final makeListTile = ListTile(
+        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        leading: Container(
+          padding: EdgeInsets.only(right: 12.0),
+          decoration: new BoxDecoration(
+              border: new Border(
+                  right: new BorderSide(width: 1.0, color: Colors.white24))),
+          child: Icon(Icons.autorenew, color: Colors.white),
+        ),
+        title: Text(
+          "Introduction to Driving",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+        subtitle: Row(
+          children: <Widget>[
+            Icon(Icons.linear_scale, color: Colors.yellowAccent),
+            Text(" Intermediate", style: TextStyle(color: Colors.white))
+          ],
+        ),
+        trailing:Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
+    final makeCard = Card(
+      elevation: 8.0,
+      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+      child: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+        child: makeListTile,
+      ),
+    );
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0.1,
+        backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
+        title: Text('Điểm danh'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return makeCard;
+          },
+        ),
+      ),
+    );
+  }
+}
